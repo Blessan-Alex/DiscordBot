655db5bfcf76c212308b5492bcf2df6a845ba6d0
Blessan-Alex <bloxgames7@gmail.com>
2021-01-09 23:41:18 +0530

    changed defective music command

--- BOT.py
+++ BOT.py
@@ -1,27 +1,20 @@
 import discord
+import random
 import asyncio
 from discord.embeds import Embed
 from discord.ext import commands
 import random
 import os
 import youtube_dl
-import json
-import requests
-import datetime
-import configparser
-import aiohttp
-import sqlite3
 
+
+
+hugimage=['https://www.clipartkey.com/view/Jbwxib_milkandmocha-hug-cute-bears-happy-kawaii-freetoedit-milk/']
 filtered_words=["Blessan sucks"]
 joined=0
 
-config = configparser.ConfigParser()
-config.read('keys.ini')
 
 client = commands.Bot(command_prefix = '=')
-
-apikey = config['Tenor']['key']
-
 client.remove_command('help')
 
 
@@ -119,100 +112,14 @@ async def Hi(ctx):
 async def Bye(ctx):
     await ctx.send("You are going?:(")
 
-@client.event
-async def on_message(ctx):
-    if ctx.content.startswith("tenor.search"):
-            message_content = ctx.content
-            split_message = message_content.split()
-            len_message = int(len(split_message)) - 1
-            datetime_now = datetime.datetime.now()
-            time = datetime_now.strftime("%d/%m/%Y %H:%M:%S")
-            try:
-                int(split_message[len_message])
-                check = True
-            except:
-                check = False
-            if check == False:
-                search = "https://api.tenor.com/v1/search?q{}&key={}&limit=1&media_filter=basic".format(split_message[1:], apikey)
-                get = requests.get(search)
-                if get.status_code == 200:
-                    json_search = get.json()
-                    json_check = json_search['next']
-                    if json_check == "0":
-                        await ctx.channel.send("{} I didn't found any gifs".format(ctx.author.mention))
-                    else:
-                        json_s = json_search['results']
-                        table = json_s[0]
-                        title = table["title"]
-                        ID = table['id']
-                        url = json_search['results']
-                        url = url[0]
-                        url = url['url']
-                        shares = json_search['results']
-                        shares = shares[0]
-                        shares = shares['shares']
-                        table = table.get("media")
-                        table = table[0]
-                        table = table.get("gif")
-                        table = table.get("url")
-                        if title == "":
-                            title = "None"
-                        search_embed = discord.Embed(title="Search Results", colour=discord.Color.blue(), image="https://i.ibb.co/dKtMKmG/tenor-logo.jpg")
-                        search_embed.add_field(name="Title: ", value=title, inline=False)
-                        search_embed.add_field(name="ID: ", value=ID, inline=False)
-                        search_embed.add_field(name="Link: ", value=url, inline=True)
-                        search_embed.set_image(url=table)
-                        search_embed.set_footer(text="You sent requests at {}".format(time), icon_url="https://i.ibb.co/dKtMKmG/tenor-logo.jpg")
-                        search_embed.set_thumbnail(url="https://i.ibb.co/dKtMKmG/tenor-logo.jpg")
-                        await ctx.channel.send(embed=search_embed)
-                elif get.status_code == 404:
-                    await ctx.channel.send("{} Tenor is working at the moments now!".format(ctx.author.mention))	
-            elif check == True:
-                search_limit = split_message[len_message]
-                split_message.pop()
-                search = "https://api.tenor.com/v1/search?q={}&key={}&limit={}&media_filter=basic".format(split_message[1:], apikey, search_limit)
-                get = requests.get(search)
-                if get.status_code == 200:
-                    json_s = get.json()
-                    json_check = json_s["next"]
-                    if json_check == "0":
-                        await ctx.channel.send("{} I didn't found any gifs".format(ctx.author.mention))
-                    elif json_check < search_limit:
-                        await ctx.channel.send("{} The maximum of this search is {}!".format(ctx.author.mention, json_check))
-                    else:
-                        json_s = json_s["results"]	
-                        i = 0
-                        while i <= int(search_limit):
-                            table = json_s[i]
-                            title = table['title']
-                            ID = table['id']
-                            url = json_s[i]
-                            url = url['url']
-                            table = table.get("media")
-                            table = table[0]
-                            table = table.get("gif")
-                            table = table.get("url")
-                            #await message.channel.send("{}".format(table))
-                            if title == "":
-                                title = "None"
-                            i += 1
-                            search_embed = discord.Embed(title="Search Results", colour=discord.Color.blue(), image="https://i.ibb.co/dKtMKmG/tenor-logo.jpg")
-                            search_embed.add_field(name="Title: ", value=title, inline=False)
-                            search_embed.add_field(name="ID: ", value=ID, inline=False)
-                            search_embed.add_field(name="Link: ", value=url, inline=True)
-                            search_embed.set_image(url=table)
-                            search_embed.set_footer(text="You sent requests at {}".format(time), icon_url="https://i.ibb.co/dKtMKmG/tenor-logo.jpg")
-                            search_embed.set_thumbnail(url="https://i.ibb.co/dKtMKmG/tenor-logo.jpg")
-                            await ctx.channel.send(embed=search_embed)
-                            if i == int(search_limit):
-                                break
-                            else:
-                                continue			
-                elif get.status_code == 404:
-                    await ctx.channel.send("{} Tenor is working at the problems now!".format(ctx.author.mention))
-            elif split_message[len_message] == 50:
-                await ctx.channel.send("{} It's to much gifs in 1 time!".format(ctx.author.mention))
 
+#sending images command
+@client.command()
+async def meme(ctx):
+    embed=discord.Embed(color=discord.Colour.red())
+    random_link=random.choice(hugimage)
+    embed.set_image(url=random_link)
+    await ctx.send(embed=embed)
 
 
 #kick command
@@ -282,37 +189,10 @@ async def whois(ctx,member:discord.Member):
     await ctx.send(embed=embed)
 
 
-#music commandss
-@client.command()
-async def play(ctx,url : str,channel):
-    song_there=os.path.isfile('song.mp3')
-    try:
-        if song_there:
-            os.remove('song.mp3')
-    except PermissionError:
-        await ctx.send('Wait for the current music to end or us the <stop> command')
-        return
-
-    voiceChannel= discord.utils.get(ctx.guild.voice_channels, name=channel)
-    await voiceChannel.connect()
-    voice =discord.utils.get(client.voice_clients,guild=ctx.guild)
 
 
-    ydl_opts={
-        'format': 'bestaudio/best',
-        'postprocessors' : [{
-            'key':'FFmpegExtractAudio',
-            'prefferedcodec':'mp3',
-            'prefferedquality':'192'
-        }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    for file in os.listdir('./'):
-        if file.endswith('.mp3'):
-            os.rename(file,'song.mp3')
-    voice.play(discord.FFmpegPCMAudio('song.mp3'))
     
+
 
 @client.command()
 async def leave(ctx):
@@ -374,4 +254,7 @@ async def clear(ctx,error):
     
 #error messages end
 
-client.run(config['Discord']['key'])
+
+
+
+client.run('Your token')
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python3 BOT.py
+worker: python BOT.py
